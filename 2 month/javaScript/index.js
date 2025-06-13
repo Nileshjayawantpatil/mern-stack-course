847b8c9962307ab4caa86b36e1d0d8dff7ef035f
Nilesh jayawant patil <np298267@gmail.com>
2025-06-13 16:31:38 +0530

    done with changes

--- 2 month/javaScript/index.js
+++ 2 month/javaScript/index.js
@@ -1,2 +1,14 @@
-console.log("Hari om")
+console.log("My name is Hari om") 
 
+function add(a,b){
+    // This function adds two number and return result
+    return a + b;
+    console .log(add(3,6));
+
+}
+
+function multiply(c,d){
+    return c*d;
+    console.log(multiply(2,6));
+
+} 
