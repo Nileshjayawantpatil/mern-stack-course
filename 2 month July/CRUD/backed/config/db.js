c921913ee017aac00270285b648445aafbe2183d
Nilesh jayawant patil <np298267@gmail.com>
2025-06-29 12:40:05 +0530

    Done backned

---
+++ 2 month July/CRUD/backed/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const connectMongoDb = async () => {
+    try {
+         await mongoose.connect("mongodb://127.0.0.1:27017/school-database");
+         console.log("connected db ..");
+
+
+
+
+    }
+    catch (error) {
+        console.log(error);
+        
+
+    }
+}
+
+module.exports = connectMongoDb 
---
+++ 2 month July/CRUD/backed/index.js
@@ -0,0 +1,33 @@
+// Import required modules
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const connectMongoDB = require("./config/db");
+const Student = require("./models/studentModel");
+const { CreateStudent, getAllStudent } = require("./Controller/students");
+
+// Create an Express application
+const app = express();
+
+
+
+
+// Port where the server will run
+
+
+app.listen(8080,() => {
+  console.log("hello node api");
+});
+// Middleware to parse JSON data from requests
+app.use(bodyParser.json());
+
+// connect mongoDB
+
+connectMongoDB()
+
+// CREATE - Add a new student (POST /students)
+app.post("/students",CreateStudent);
+ 
+
+// READ ALL - Get all students (GET /students)
+app.get("/students",getAllStudent);
